a3da1400ead96132cda4c3e64d48c08d176d0336
Carlos Joseph Dizon <carlosjosephdizon19@gmail.com>
2025-05-05 19:16:10 +0800

    DEELTE ALL

--- dj.php
+++
@@ -1,9 +0,0 @@
-<?php
-// This is a simple PHP "Hello, World!" program.
-
-// The echo statement is used to output text to the browser.
-echo "Dj!";
-
-// You can also use single quotes, but double quotes allow for variable interpolation.
-// echo 'Hello, World!';
-?>
