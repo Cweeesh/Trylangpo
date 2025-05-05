434ccde1936ce815f9bd999488e60b94c51fec1e
Carlos Joseph Dizon <carlosjosephdizon19@gmail.com>
2025-05-05 18:13:56 +0800

    Panibago.php

---
+++ panibago.php
@@ -0,0 +1,9 @@
+<?php
+// This is a simple PHP "Hello, World!" program.
+
+// The echo statement is used to output text to the browser.
+echo "panibago!";
+
+// You can also use single quotes, but double quotes allow for variable interpolation.
+// echo 'Hello, World!';
+?>
